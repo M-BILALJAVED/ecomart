e0c1c33f77dd4c62053b71b205f663a289945fe6
bilal <m-bilaljaved612@gmail.com>
2024-08-06 18:55:01 +0500

    add product feature

--- javascript/main.js
+++ javascript/main.js
@@ -1,65 +1,83 @@
 // Initialize Swiper 
-    var swiper = new Swiper(".mySwiper", {
-        spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: {
-            delay: 9999991500,
-            disableOnInteraction: false,
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-    });
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9999991500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
-    // for tabs
-    var tabButtons = document.querySelectorAll(".tablinks");
+// for tabs
+var tabButtons = document.querySelectorAll(".tablinks");
 
-    for (var i = 0; i < tabButtons.length; i++) {
-      tabButtons[i].addEventListener("click", function () {
-        var tabName = this.dataset.tab;
-        var tabContent = document.getElementById(tabName);
-        var allTabContent = document.querySelectorAll(".tabcontent");
-        var allTabButtons = document.querySelectorAll(".tablinks");
+for (var i = 0; i < tabButtons.length; i++) {
+  tabButtons[i].addEventListener("click", function () {
+    var tabName = this.dataset.tab;
+    var tabContent = document.getElementById(tabName);
+    var allTabContent = document.querySelectorAll(".tabcontent");
+    var allTabButtons = document.querySelectorAll(".tablinks");
 
-        for (var j = 0; j < allTabContent.length; j++) {
-          allTabContent[j].style.display = "none";
-        }
-
-        for (var k = 0; k < allTabButtons.length; k++) {
-          allTabButtons[k].classList.remove("active");
-        }
-
-        tabContent.style.display = "block";
-        this.classList.add("active");
-      });
+    for (var j = 0; j < allTabContent.length; j++) {
+      allTabContent[j].style.display = "none";
     }
 
-    document.querySelector(".tablinks").click();
+    for (var k = 0; k < allTabButtons.length; k++) {
+      allTabButtons[k].classList.remove("active");
+    }
+
+    tabContent.style.display = "block";
+    this.classList.add("active");
+  });
+}
+
+document.querySelector(".tablinks").click();
 
 
 
-    // 2nd slider
+// 2nd slider
 
-    var swiper = new Swiper(".mySwiper2", {
-        slidesPerView: 1,
-        spaceBetween: 5,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        breakpoints: {
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 4,
-            spaceBetween: 40,
-          },
-          1024: {
-            slidesPerView: 5,
-            spaceBetween: 50,
-          },
-        },
-      });
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: 1,
+  spaceBetween: 5,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+
+// 3rd slider
+
+
+var swiper = new Swiper(".mySwiper3", {
+  slidesPerView: 1,
+  spaceBetween: 5,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
